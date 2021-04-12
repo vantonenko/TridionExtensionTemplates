eb24fd3315fd2be658d47dfa0d90fea4445c3a2f
Andriy Shatrov <ashatrov@sdl.com>
2021-04-12 16:37:02 +0300

    Added Resolver Extension, shifted to 1.1 version with 9.6 versions inside.

---
+++ ResolverExtension/ResolverExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Tridion.ContentManager;
+using Tridion.ContentManager.Publishing;
+using Tridion.ContentManager.Publishing.Resolving;
+
+namespace ResolverExtension
+{
+    public class ResolverExtension
+    {
+        #region IResolver Members
+        // This Resolve method executes after the default resolver has done its job and produced a set of resolved items.
+        public void Resolve(IdentifiableObject item, ResolveInstruction instruction, PublishContext context, ISet<ResolvedItem> resolvedItems)
+        {
+            //resolvedItems.Add(...);
+        }
+        #endregion
+    }
+}
